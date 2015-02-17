927c7128cb1714a269fa21206f27dc47b10641b2
eabderh <eabderh@clemson.edu>
2015-02-17 15:14:51 -0500

    deleted files

--- elias.js
+++
@@ -1,7 +0,0 @@
-if (1) (function() {
-    'use strict';
-    var l = document.createElement('link');
-    l.rel = 'stylesheet';
-    l.href = chrome.runtime.getURL('Custom.css');
-    document.head.appendChild(l);
-})();
