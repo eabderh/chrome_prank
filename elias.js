1cb1a82c8b2a58fea4ade09fa8e99de5d69822d9
eabderh <eabderh@clemson.edu>
2015-02-17 15:02:10 -0500

    initial commit

---
+++ elias.js
@@ -0,0 +1,7 @@
+if (1) (function() {
+    'use strict';
+    var l = document.createElement('link');
+    l.rel = 'stylesheet';
+    l.href = chrome.runtime.getURL('Custom.css');
+    document.head.appendChild(l);
+})();
